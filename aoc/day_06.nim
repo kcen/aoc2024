89af2af2bf04c8518a377c595907bc1c8dc36c0c
Casey <kcen@noreply.github.com>
2024-12-06 01:52:20 -0800

    a little cleanup

--- aoc/day_06.nim
+++ aoc/day_06.nim
@@ -1,12 +1,194 @@
+discard """
+--- Day 6: Guard Gallivant ---
+The Historians use their fancy device again, this time to whisk you all away to the North Pole prototype suit manufacturing lab... in the year 1518! It turns out that having direct access to history is very convenient for a group of historians.
 
-import std/[strutils, strscans, sequtils, sugar, math, algorithm, tables]
-import aoc_utils, itertools
+You still have to be careful of time paradoxes, and so it will be important to avoid anyone from 1518 while The Historians search for the Chief. Unfortunately, a single guard is patrolling this part of the lab.
+
+Maybe you can work out where the guard will go ahead of time so that The Historians can search safely?
+
+You start by making a map (your puzzle input) of the situation. For example:
+
+....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...
+The map shows the current position of the guard with ^ (to indicate the guard is currently facing up from the perspective of the map). Any obstructions - crates, desks, alchemical reactors, etc. - are shown as #.
+
+Lab guards in 1518 follow a very strict patrol protocol which involves repeatedly following these steps:
+
+If there is something directly in front of you, turn right 90 degrees.
+Otherwise, take a step forward.
+Following the above protocol, the guard moves up several times until she reaches an obstacle (in this case, a pile of failed suit prototypes):
+
+....#.....
+....^....#
+..........
+..#.......
+.......#..
+..........
+.#........
+........#.
+#.........
+......#...
+Because there is now an obstacle in front of the guard, she turns right before continuing straight in her new facing direction:
+
+....#.....
+........>#
+..........
+..#.......
+.......#..
+..........
+.#........
+........#.
+#.........
+......#...
+Reaching another obstacle (a spool of several very long polymers), she turns right again and continues downward:
+
+....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#......v.
+........#.
+#.........
+......#...
+This process continues for a while, but the guard eventually leaves the mapped area (after walking past a tank of universal solvent):
+
+....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#........
+........#.
+#.........
+......#v..
+By predicting the guard's route, you can determine which specific positions in the lab will be in the patrol path. Including the guard's starting position, the positions visited by the guard before leaving the area are marked with an X:
+
+....#.....
+....XXXXX#
+....X...X.
+..#.X...X.
+..XXXXX#X.
+..X.X.X.X.
+.#XXXXXXX.
+.XXXXXXX#.
+#XXXXXXX..
+......#X..
+In this example, the guard will visit 41 distinct positions on your map.
+
+Predict the path of the guard. How many distinct positions will the guard visit before leaving the mapped area?
+
+Your puzzle answer was 5145.
+
+--- Part Two ---
+While The Historians begin working around the guard's patrol route, you borrow their fancy device and step outside the lab. From the safety of a supply closet, you time travel through the last few months and record the nightly status of the lab's guard post on the walls of the closet.
+
+Returning after what seems like only a few seconds to The Historians, they explain that the guard's patrol area is simply too large for them to safely search the lab without getting caught.
+
+Fortunately, they are pretty sure that adding a single new obstruction won't cause a time paradox. They'd like to place the new obstruction in such a way that the guard will get stuck in a loop, making the rest of the lab safe to search.
+
+To have the lowest chance of creating a time paradox, The Historians would like to know all of the possible positions for such an obstruction. The new obstruction can't be placed at the guard's starting position - the guard is there right now and would notice.
+
+In the above example, there are only 6 different positions where a new obstruction would cause the guard to get stuck in a loop. The diagrams of these six situations use O to mark the new obstruction, | to show a position where the guard moves up/down, - to show a position where the guard moves left/right, and + to show a position where the guard moves both up/down and left/right.
+
+Option one, put a printing press next to the guard's starting position:
+
+....#.....
+....+---+#
+....|...|.
+..#.|...|.
+....|..#|.
+....|...|.
+.#.O^---+.
+........#.
+#.........
+......#...
+Option two, put a stack of failed suit prototypes in the bottom right quadrant of the mapped area:
+
+
+....#.....
+....+---+#
+....|...|.
+..#.|...|.
+..+-+-+#|.
+..|.|.|.|.
+.#+-^-+-+.
+......O.#.
+#.........
+......#...
+Option three, put a crate of chimney-squeeze prototype fabric next to the standing desk in the bottom right quadrant:
+
+....#.....
+....+---+#
+....|...|.
+..#.|...|.
+..+-+-+#|.
+..|.|.|.|.
+.#+-^-+-+.
+.+----+O#.
+#+----+...
+......#...
+Option four, put an alchemical retroencabulator near the bottom left corner:
+
+....#.....
+....+---+#
+....|...|.
+..#.|...|.
+..+-+-+#|.
+..|.|.|.|.
+.#+-^-+-+.
+..|...|.#.
+#O+---+...
+......#...
+Option five, put the alchemical retroencabulator a bit to the right instead:
+
+....#.....
+....+---+#
+....|...|.
+..#.|...|.
+..+-+-+#|.
+..|.|.|.|.
+.#+-^-+-+.
+....|.|.#.
+#..O+-+...
+......#...
+Option six, put a tank of sovereign glue right next to the tank of universal solvent:
+
+....#.....
+....+---+#
+....|...|.
+..#.|...|.
+..+-+-+#|.
+..|.|.|.|.
+.#+-^-+-+.
+.+----++#.
+#+----++..
+......#O..
+It doesn't really matter what you choose to use as an obstacle so long as you and The Historians can put it into position without the guard noticing. The important thing is having enough options that you can find one that minimizes time paradoxes, and in this example, there are 6 different positions you could choose.
+
+You need to get the guard stuck in a loop by adding a single new obstruction. How many different positions could you choose for this obstruction?
+
+Your puzzle answer was 1523.
+"""
+import std/[strutils, sequtils, algorithm, sets]
+import aoc_utils
 
 #Position is bigger Row == Down, bigger Column == Right
 # These correspond to Up, Right, Left, Down TODO: ENUM
 const directions = @[(-1, 0), (0, 1), (1, 0), (0, -1)]
 type Pos = (int, int)
 var max: Pos
+var artifacts: seq[Pos] = @[]
 
 func `+`(x, y: Pos): Pos =
   (x[0] + y[0], x[1] + y[1])
@@ -16,11 +198,35 @@ proc inbounds(max: Pos, current: Pos): bool =
     return false
   return true
 
-proc day_06*(): Solution =
-  var artifacts: seq[Pos] = @[]
-  var start_guard_Position: Pos
-  var visited = initTable[Pos, int]()
+proc isLoop(start: Pos, extra_value: (int, int) = (-1, -1)): bool =
+  var visited = initHashSet[(Pos, int)]()
+  var pos = start
+  var bearing = 0
+  while max.inbounds(pos):
+    var next_pos = pos + directions[bearing]
+    if artifacts.contains(next_pos) or next_pos == extra_value:
+      bearing = (bearing + 1) mod 4
+      continue
+    if visited.containsOrIncl((pos, bearing)):
+      return true
+    pos = next_pos
+  false
 
+proc getVisited(start: Pos): seq[Pos] =
+  var visited = initHashSet[Pos]()
+  var pos = start
+  var bearing = 0
+  while max.inbounds(pos):
+    var next_pos = pos + directions[bearing]
+    if artifacts.contains(next_pos):
+      bearing = (bearing + 1) mod 4
+      continue
+    visited.incl(pos)
+    pos = next_pos
+  return sorted(visited.items.toSeq()).deduplicate(true)
+
+proc day_06*(): Solution =
+  var start_guard_Position: Pos
   var charMap = getInput().splitlines.mapIt(cast[seq[char]](it))
   for row, line_data in charMap:
     for column, value in line_data.pairs:
@@ -32,39 +238,10 @@ proc day_06*(): Solution =
       else:
         continue
   max = (len(charMap) - 1, len(charMap[0]) - 1)
-  var heading = 0
-  var guard_Position = start_guard_Position
-  while max.inbounds(guard_Position):
-    visited[guard_Position] = heading
-    var next_Position = guard_Position + directions[heading]
-    if artifacts.contains(next_Position):
-      heading = (heading + 1) mod 4
-      next_Position = guard_Position + directions[heading]
-    guard_Position = next_Position
-
-  var visited_list = visited.keys.toSeq()
-  visited_list.sort()
-  let part_1_visited = visited_list.deduplicate(true)
+  let part_1_visited = start_guard_Position.getVisited()
   let pt_1 = len(part_1_visited)
   var loops_found = 0
-  var break_me = 0
-  for check_num, p in part_1_visited.pairs:
-    echo "checking :", check_num
-    break_me = 0
-    heading = 0
-    visited = initTable[Pos, int]()
-    guard_Position = start_guard_Position
-    while max.inbounds(guard_Position):
-      if visited.getOrDefault(guard_Position, -1) == heading:
-        inc(loops_found)
-        break
-      visited[guard_Position] = heading
-      var next_Position = guard_Position + directions[heading]
-      if next_Position == p or artifacts.contains(next_Position):
-        heading = (heading + 1) mod 4
-        next_Position = guard_Position + directions[heading]
-      guard_Position = next_Position
-      inc(break_me)
-      if break_me > 10000:
-        break
+  for idx, p in part_1_visited.pairs:
+    if start_guard_Position.isLoop(p):
+      inc(loops_found)
   Solution(part_one: $pt_1, part_two: $loops_found)
