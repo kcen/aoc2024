1050037ea67521e889b545f40111f02c417d9771
Casey <kcen@noreply.github.com>
2024-12-14 17:27:53 -0800

    day 14 improved

--- aoc.nim
+++ aoc.nim
@@ -1,4 +1,4 @@
-import aoc/[aoc_utils, day_00, day_01, day_02, day_03, day_04, day_05, day_06, day_07, day_08, day_09, day_10, day_11, day_12, day_13]
+import aoc/[aoc_utils, day_00, day_01, day_02, day_03, day_04, day_05, day_06, day_07, day_08, day_09, day_10, day_11, day_12, day_13, day_14]
 when compileOption("profiler"):
   import nimprof
 
@@ -31,5 +31,7 @@ case getDay():
     printSolution day_12()
   of 13:
     printSolution day_13()
+  of 14:
+    printSolution day_14()
   else:
     notImplemented()
